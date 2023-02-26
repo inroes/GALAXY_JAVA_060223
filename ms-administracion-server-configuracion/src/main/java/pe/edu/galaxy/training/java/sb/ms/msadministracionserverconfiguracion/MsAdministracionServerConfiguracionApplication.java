3b817492f9a533f887f8fa2a0beb215350d36330
inroes <inroes.soluciones@gmail.com>
2023-02-26 06:35:37 -0500

    Microservices - Config Server

--- ms-administracion-server-configuracion/src/main/java/pe/edu/galaxy/training/java/sb/ms/msadministracionserverconfiguracion/MsAdministracionServerConfiguracionApplication.java
+++ ms-administracion-server-configuracion/src/main/java/pe/edu/galaxy/training/java/sb/ms/msadministracionserverconfiguracion/MsAdministracionServerConfiguracionApplication.java
@@ -2,7 +2,9 @@ package pe.edu.galaxy.training.java.sb.ms.msadministracionserverconfiguracion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class MsAdministracionServerConfiguracionApplication {
 
